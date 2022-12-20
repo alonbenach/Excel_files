d8555c870697608134acdd1c59022ee3a396f558
alonbenach <benach@gmail.com>
2022-12-20 13:49:59 +0200

    update 20.12.2022 20:14

--- integrated_datasets.R
+++ integrated_datasets.R
@@ -1,2 +1,11 @@
 ##integrated datasets
 
+install.packages("readxl")
+library(tidyverse)
+library(lubridate)
+Sys.setlocale("LC_ALL", "Polish")
+
+readxl::read_excel(path = "C:/Users/Alon/OneDrive/Docs/GitHub/Excel_files/Stores.xlsx")
+
+Stores
+read_csv("C:/Users/Alon/OneDrive/Docs/GitHub/Excel_files/Summary of Sales January 2020.csv")
