89672bc5a475d664c1fc495f6c43aa954a5780ea
alonbenach <benach@gmail.com>
2023-01-03 14:50:24 +0200

    20230103 14:50 update

--- integrated_datasets.R
+++ integrated_datasets.R
@@ -4,35 +4,41 @@ library("readxl")
 library("tidyverse")
 library("tidyselect")
 
-
 Sys.setlocale("LC_ALL", "Polish")
+Sys.getlocale()
 
 stores <- readxl::read_excel(path = "C:/Users/Alon/OneDrive/Docs/GitHub/Excel_files/Stores.xlsx")
 
-salesJan2020 <- read_csv("C:/Users/Alon/OneDrive/Docs/GitHub/Excel_files/Summary of Sales January 2020.csv")
+sales <- read_csv("C:/Users/Alon/OneDrive/Docs/GitHub/Excel_files/Summary of Sales January 2020.csv")
 
 #truncating store names
-salesJan2020$'STORE NAME' <- gsub(' GROSS', '', salesJan2020$'STORE NAME')
-salesJan2020$'STORE NAME' <- gsub(' OTHER', '', salesJan2020$'STORE NAME')
-salesJan2020$'STORE NAME' <- gsub(' RETAIL', '', salesJan2020$'STORE NAME')
+truncate_sales <- function(df) {
+  df$'STORE NAME' <- gsub(' GROSS', '', df$'STORE NAME')
+  df$'STORE NAME' <- gsub(' OTHER', '', df$'STORE NAME')
+  df$'STORE NAME' <- gsub(' RETAIL', '', df$'STORE NAME')
+  df$'STORE NAME' <- gsub("\xd3", 'Ó', df$'STORE NAME')
+}
+
+sales$'STORE NAME' <- truncate_sales(sales)
 
 #replacing false names with proper ones from the file
 
-salesJan2020[salesJan2020=="PARVIFLORA ?OM?A"] <- "Parviflora Łomża"
-salesJan2020[salesJan2020=="PARVIFLORA WROC?AW"] <- "Parviflora Wrocław"
-salesJan2020[salesJan2020=="PARVIFLORA ?\xd3D?"] <- "Parviflora Łódź" #problem with identifying the false name - Ó
-salesJan2020[salesJan2020=="PARVIFLORA POZNA?"] <- "Parviflora Poznań"
-salesJan2020[salesJan2020=="PARVIFLORA KRAKOW"] <- "Parviflora Kraków" #problem with identifying the false name Ó
-salesJan2020[salesJan2020=="PARVIFLORA GDA?SK"] <- "Parviflora Gdańsk"
-salesJan2020[salesJan2020=="PARVIFLORA CHE?M"] <- "Parviflora Chełm"
-salesJan2020[salesJan2020=="PARVIFLORA BIA?YSTOK"] <- "Parviflora Białystok"
-salesJan2020[salesJan2020=="PARVIFLORA SUWA?KI"] <- "Parviflora Suwałki"
-salesJan2020[salesJan2020=="PARVIFLORA TORU?"] <- "Parviflora Toruń"
-salesJan2020[salesJan2020=="PARVIFLORA GORZOW WLKP."] <- "Parviflora Gorzów Wlkp." #problem with identifying the false name Ó
-salesJan2020[salesJan2020=="PARVIFLORA PRZEMY?L"] <- "Parviflora Przemyśl"
-salesJan2020[salesJan2020=="PARVIFLORA RZESZOW"] <- "Parviflora Rzeszów"
-salesJan2020[salesJan2020=="PARVIFLORA OSTRO??KA"] <- "Parviflora Ostrołęka"
-salesJan2020[salesJan2020=="PARVIFLORA W?CHOCK"] <- "Parviflora Wąchock"
-salesJan2020[salesJan2020=="PARVIFLORA ?WIEBODZIN"] <- "Parviflora Świebodzin"
+sales[sales=="PARVIFLORA ?OM?A"] <- "Parviflora Łomża"
+sales[sales=="PARVIFLORA WROC?AW"] <- "Parviflora Wrocław"
+sales[sales=="PARVIFLORA ?\xd3D?"] <- "Parviflora Łódź" #problem with identifying the false name - Ó
+sales[sales=="PARVIFLORA POZNA?"] <- "Parviflora Poznań"
+sales[sales=="PARVIFLORA KRAK\xd3W"] <- "Parviflora Kraków" #problem with identifying the false name Ó
+sales[sales=="PARVIFLORA GDA?SK"] <- "Parviflora Gdańsk"
+sales[sales=="PARVIFLORA CHE?M"] <- "Parviflora Chełm"
+sales[sales=="PARVIFLORA BIA?YSTOK"] <- "Parviflora Białystok"
+sales[sales=="PARVIFLORA SUWA?KI"] <- "Parviflora Suwałki"
+sales[sales=="PARVIFLORA TORU?"] <- "Parviflora Toruń"
+sales[sales=="PARVIFLORA GORZ\xd3W WLKP."] <- "Parviflora Gorzów Wlkp." #problem with identifying the false name Ó
+sales[sales=="PARVIFLORA PRZEMY?L"] <- "Parviflora Przemyśl"
+sales[sales=="PARVIFLORA RZESZ\xd3W"] <- "Parviflora Rzeszów"
+sales[sales=="PARVIFLORA OSTRO??KA"] <- "Parviflora Ostrołęka"
+sales[sales=="PARVIFLORA W?CHOCK"] <- "Parviflora Wąchock"
+sales[sales=="PARVIFLORA ?WIEBODZIN"] <- "Parviflora Świebodzin"
 
-salesJan2020$'STORE NAME'
+sales$'STORE NAME'
+
